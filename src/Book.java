82a3912fbe23bfcacfcc8961491164ccf147260f
roman <roman.pshenichnikov555@gmail.com>
2022-09-29 20:57:13 +0300

    hw9.1 done

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,27 @@
 public class Book {
+    private final Author author;
+    String nameOfTheBook;
+    int theYearOfPublishing;
+    public Book (Author author, String nameOfTheBook, int theYearOfPublishing) {
+        this.author = author;
+        this.nameOfTheBook = nameOfTheBook;
+        this.theYearOfPublishing = theYearOfPublishing;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public String getNameOfTheBook() {
+        return this.nameOfTheBook;
+    }
+
+    public int getTheYearOfPublishing() {
+        return this.theYearOfPublishing;
+    }
+
+    public void setTheYearOfPublishing(int theYearOfPublishing) {
+        this.theYearOfPublishing = theYearOfPublishing;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
+    public static void separation() {
+        System.out.println("========================================================");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        separation();
+        Author tolstoi = new Author("Lev","Tolstoi");
+        Author pushkin = new Author("Alexander","Pushkin");
+        Book warAndPeace = new Book(tolstoi, "War and Peace", 0);
+        Book captainsDaugher = new Book(pushkin, "Captain's Daugher", 0);
+        System.out.println("warAndPeace.getAuthor().getFirtsName() = " + warAndPeace.getAuthor().getFirtsName());
+        System.out.println("warAndPeace.getAuthor().getLastName() = " + warAndPeace.getAuthor().getLastName());
+        System.out.println("warAndPeace.getNameOfTheBook() = " + warAndPeace.getNameOfTheBook());
+        warAndPeace.setTheYearOfPublishing(1865);
+        System.out.println("warAndPeace.getTheYearOfPublishing() = " + warAndPeace.getTheYearOfPublishing());
+        separation();
+        System.out.println("captainsDaugher.getAuthor().getFirtsName() = " + captainsDaugher.getAuthor().getFirtsName());
+        System.out.println("captainsDaugher.getAuthor().getLastName() = " + captainsDaugher.getAuthor().getLastName());
+        System.out.println("captainsDaugher.getNameOfTheBook() = " + captainsDaugher.getNameOfTheBook());
+        captainsDaugher.setTheYearOfPublishing(1836);
+        System.out.println("captainsDaugher.getTheYearOfPublishing() = " + captainsDaugher.getTheYearOfPublishing());
+        separation();
+
+
     }
 }
